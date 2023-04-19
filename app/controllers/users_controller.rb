634ec9977624767dba467584800d66b367dc07dd
Dustin Doyle <doyledustin9@gmail.com>
2023-04-19 17:10:41 +0000

    everything wors except forms and deletion

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,10 +2,23 @@ class UsersController < ApplicationController
   def index
 
     matching_users = User.all
-    
+
     @list_of_users = matching_users.order({ username: :asc })
 
     render({ template: "user_templates/index.html.erb" })
 
+  end
+
+  def show
+
+    user_key = params.fetch("path_user")
+
+    @path_user = User.where({ username: user_key }).first
+
+    if @path_user == nil
+      redirect_to("/404")
+    else
+      render({ template: "user_templates/show.html.erb" })
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
 
 get("/users", { controller: "users", action: "index" })
 
-get("/users/:")
+get("/users/:path_user", { controller: "users", action: "show" })
 
+get("/photos", { controller: "photos", action: "index" })
+
+get("/photos/:path_photo", { controller: "photos", action: "show" })
 end
